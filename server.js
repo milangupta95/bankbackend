449832b267841a98bbdc42d9bd85ba4fff3ea74e
milangupta95 <milangupta95@gmail.com>
2022-09-17 21:11:56 +0530

    Added

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,           
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,           
+//     optionSuccessStatus:200
+// }
+app.use(cors());
 app.use(express.json());
 const createuserController = require('./Controllers/createUserController');
 const viewAllCustomer = require('./Controllers/viewAllController');
