aeae431b1b27b5957e5eae25ad054805ad268261
milangupta95 <milangupta95@gmail.com>
2022-09-17 16:11:17 +0530

    BackEnd Ready

---
+++ Controllers/transferAmmountController.js
@@ -0,0 +1,44 @@
+const userModel = require('../Models/userModel');
+const mongoose = require('mongoose');
+
+async function transferAmmount(req,res) {
+    try {
+        let transferData = req.body;
+        let senderUserId = transferData.senderId;
+        let recieverUserId = transferData.recieverId;
+        let ammountToBeTransfer = transferData.ammount;
+        let sender = await userModel.findById(senderUserId);
+        let reciever = await userModel.findById(recieverUserId);
+        if(sender && reciever){
+            if(sender.Balance < ammountToBeTransfer) {
+                res.status(500).json({
+                    message: "Insufficient Balance"
+                })
+            } else {
+                let oldSenderBalance = sender.Balance;
+                let newSenderBalance = oldSenderBalance - Number(ammountToBeTransfer);
+                let oldReciverBalance = reciever.Balance;
+                let newReciverBalance = oldReciverBalance + Number(ammountToBeTransfer);
+                sender.Balance = newSenderBalance;
+                reciever.Balance = newReciverBalance;
+                await sender.save();
+                await reciever.save();
+                res.status(200).json({
+                    message: "Updated SuccessFully",
+                    sender: sender,
+                    reciever: reciever
+                })
+            }
+        } else {
+            res.status(400).json({
+                message: "Sender or Reciever Not Found"
+            })
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+module.exports = transferAmmount;
---
+++ Controllers/viewAllController.js
@@ -0,0 +1,21 @@
+const userModel = require('../Models/userModel');
+const mongoose = require('mongoose');
+
+async function viewAllCustomer(req,res) {
+    try{
+        let users = await userModel.find();
+        if(users) {
+            res.status(200).send(users);
+        } else {
+            res.status(400).json({
+                message: "Could Not Fetch Data"
+            });
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+}
+
+module.exports = viewAllCustomer;
