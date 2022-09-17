9c4d97e31f12799ed0f3ec1920942073b995e2b3
milangupta95 <milangupta95@gmail.com>
2022-09-17 21:01:31 +0530

    Added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const corsOptions ={
-    origin:'http://localhost:3001/', 
+    origin:'http://localhost:3000', 
     credentials:true,           
     optionSuccessStatus:200
 }
