ab0bcb0f12377ba310f88fb891661225cdd0ef87
milangupta95 <milangupta95@gmail.com>
2022-09-17 22:50:58 +0530

    Added Upto Transactions

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ const createuserController = require('./Controllers/createUserController');
 const viewAllCustomer = require('./Controllers/viewAllController');
 const viewOneCustomer = require('./Controllers/viewOneCustomer');
 const transferAmmount = require('./Controllers/transferAmmountController');
+const getAllTransferData = require('./Controllers/transferDataController');
 app.post("/signup",createuserController);
 app.get("/viewAllCustomer",viewAllCustomer);
 app.get("/viewOneCustomer/:id",viewOneCustomer);
 app.patch("/transferAmmount",transferAmmount);
+app.get("/transactions",getAllTransferData);
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT,function() {
     console.log("SuccessFully Connected to 3000 Port");
 });
